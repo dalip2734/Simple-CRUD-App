f8a6b1cc1595acdb2ba3a074c35a1c351b8279f4
Dalip Yadav <dalipyadav68@gmail.com>
2024-02-19 23:02:20 +0530

    Create and Retrieve data through API

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const app = express();
 
-app.listen(3000,()=> {
-    console.log("SERVER is running on port 3000");
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello From NODE API Server updated');
 });
 
-app.get('/',(req, res) => {
-    res.send("Hello From NODE API");
+app.get('/api/products', async (req, res)=> {
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch{
+        res.status(500).json({message:error.message});
+    }
 });
+//get request by id
+
+app.get('/api/product/:id', async (req,res)=>{
+    try{
+      const {id} = req.params;
+     const product = await Product.findById(id);
+     res.status(200).json({product});
+    }catch{
+        res.status(500).json({message:error.message});
+
+    }
+})
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://dalip01:nwosNBu777jCBpsX@backenddb.1ep2mug.mongodb.net/Node-API?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to database!');
+    app.listen(3000, () => {
+      console.log('SERVER is running on port 3000');
+    });
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
