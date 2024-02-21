fbe92898e2f921dfe85bb28ee7c845d21f4ec86a
Dalip Yadav <dalipyadav68@gmail.com>
2024-02-21 21:02:10 +0530

    Delete An API

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ app.get('/', (req, res) => {
   res.send('Hello From NODE API Server updated');
 });
 
-app.get('/api/products', async (req, res)=> {
-    try{
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch{
-        res.status(500).json({message:error.message});
-    }
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
 });
 //get request by id
 
-app.get('/api/product/:id', async (req,res)=>{
-    try{
-      const {id} = req.params;
-     const product = await Product.findById(id);
-     res.status(200).json({product});
-    }catch{
-        res.status(500).json({message:error.message});
+app.get('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json({ product });
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-    }
-})
-
+// create a Product
 app.post('/api/products', async (req, res) => {
   try {
     const product = await Product.create(req.body);
@@ -37,6 +37,38 @@ app.post('/api/products', async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+});
+
+// UPDATE a Product
+app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({message: "Product not found"});
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete a Product
+app.delete('/api/product/:id', async (req,res)=> {
+  try{
+     const {id} = req.params;
+     const product = await Product.findByIdAndDelete(id);
+     if(!product){
+      return res.status(404).json({message:"Product not found"});
+     }
+     res.status(200).json({message:"Product deleted succesfully"});
+  }catch{
+    res.status(500).json({ message: error.message });
+  }
+
 });
 
 mongoose
